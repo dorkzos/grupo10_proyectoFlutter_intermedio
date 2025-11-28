edbb62dcfccfabe91d57119e7bfa7fd8d978a5af
AlarconFernando <fermaualafer@gmail.com>
2025-11-27 23:40:45 -0400

    lo ultimo que hice de configuracion

--- configuracion.dart
+++ configuracion.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:guia_seguridad_vecinal/alarma_vecinal.dart';
 
 class Configuracion extends StatefulWidget {
   const Configuracion({super.key});
@@ -12,6 +13,113 @@ class _ConfiguracionState extends State<Configuracion> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Configuracion y Datos Personales')),
+      body: Center(
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              CircleAvatar(
+                radius: 45,
+                backgroundColor: Colors.grey,
+                foregroundColor: Colors.black,
+              ),
+              SizedBox(height: 30),
+              Text('Nombres y Apellidos'),
+              SizedBox(
+                width: 300,
+                child: TextField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: const Color.fromARGB(255, 228, 227, 227),
+
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(15),
+                      borderSide: BorderSide.none,
+                    ),
+                    suffixIcon: Icon(Icons.edit),
+                  ),
+                ),
+              ),
+              SizedBox(height: 10),
+              Text('Tipo de Sangre'),
+              SizedBox(
+                width: 300,
+                child: TextField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: const Color.fromARGB(255, 228, 227, 227),
+
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(15),
+                      borderSide: BorderSide.none,
+                    ),
+                    suffixIcon: Icon(Icons.edit),
+                  ),
+                ),
+              ),
+              SizedBox(height: 10),
+              Text('Direccion'),
+              SizedBox(
+                width: 300,
+                child: TextField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: const Color.fromARGB(255, 228, 227, 227),
+
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(15),
+                      borderSide: BorderSide.none,
+                    ),
+                    suffixIcon: Icon(Icons.edit),
+                  ),
+                ),
+              ),
+              SizedBox(height: 10),
+              Text('Contacto de emergencia'),
+              SizedBox(
+                width: 300,
+                child: TextField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: const Color.fromARGB(255, 228, 227, 227),
+
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(15),
+                      borderSide: BorderSide.none,
+                    ),
+                    suffixIcon: Icon(Icons.edit),
+                  ),
+                ),
+              ),
+              SizedBox(height: 20),
+              SizedBox(
+                width: 300,
+                height: 40,
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => AlarmaVecinal()),
+                    );
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.blue,
+                    foregroundColor: Colors.white,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadiusGeometry.circular(5),
+                    ),
+                  ),
+                  child: Text('GUARDAR'),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
